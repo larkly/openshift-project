24abdf7c9980683843feacbbe83eff7b7c0360d9
Bosse Klykken <larkly@gmail.com>
2016-10-25 15:43:38 +0200

    hreirhei

--- download.sh
+++ download.sh
@@ -7,8 +7,10 @@ HOME=/tmp
 cd /tmp
 
 cp /tmp/ffmpeg*/ffmpeg /tmp
+cp /tmp/ffmpeg*/ffprobe /tmp
 PATH=$PATH:/tmp
 chmod +x /tmp/ffmpeg
+chmod +x /tmp/ffprobe
 
 youtube-dl --audio-format mp3 -x "$YOUTUBE"
 touch /tmp/ready
